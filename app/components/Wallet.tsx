9a1f0d0f48a6369173de4f2cf5a574a00f37ed51
adityaslyf <aditya.varshneymail@gmail.com>
2024-09-09 12:18:10 +0530

    fixed some type errors

--- app/components/Wallet.tsx
+++ app/components/Wallet.tsx
@@ -1,33 +1,33 @@
-import React, { FC, useMemo } from 'react';
-import { useSearchParams } from "next/navigation";
-import { WalletProvider } from '@solana/wallet-adapter-react';
-import { TipLinkWalletAdapter } from "@tiplink/wallet-adapter";
-import {
-    WalletModalProvider,
-    WalletDisconnectButton,
-    WalletMultiButton,
-    TipLinkWalletAutoConnectV2
-} from '@tiplink/wallet-adapter-react-ui';
+// import React, { FC, useMemo } from 'react';
+// import { useSearchParams } from "next/navigation";
+// import { WalletProvider } from '@solana/wallet-adapter-react';
+// import { TipLinkWalletAdapter } from "@tiplink/wallet-adapter";
+// import {
+//     WalletModalProvider,
+//     WalletDisconnectButton,
+//     WalletMultiButton,
+//     TipLinkWalletAutoConnectV2
+// } from '@tiplink/wallet-adapter-react-ui';
 
-export const Wallet: FC = () => {    
-    const wallets = useMemo(
-        () => [
-            new TipLinkWalletAdapter({ 
-                title: "Name of Dapp", 
-                clientId: "YOUR_CLIENT_ID", // Replace with your client ID
-                theme: "dark"  // Options: "dark", "light", "system"
-            }),
-        ],
-        []
-    );
+// export const Wallet: FC = () => {    
+//     const wallets = useMemo(
+//         () => [
+//             new TipLinkWalletAdapter({ 
+//                 title: "Name of Dapp", 
+//                 clientId: "YOUR_CLIENT_ID", // Replace with your client ID
+//                 theme: "dark"  // Options: "dark", "light", "system"
+//             }),
+//         ],
+//         []
+//     );
 
-    const searchParams = useSearchParams();
+//     const searchParams = useSearchParams();
 
-    return (
-        <WalletProvider wallets={wallets} autoConnect>
-            <TipLinkWalletAutoConnectV2 isReady query={searchParams}>
-              {/* Other components go here */}
-            </TipLinkWalletAutoConnectV2>
-        </WalletProvider>
-    );
-};
+//     return (
+//         <WalletProvider wallets={wallets} autoConnect>
+//             <TipLinkWalletAutoConnectV2 isReady query={searchParams}>
+//               {/* Other components go here */}
+//             </TipLinkWalletAutoConnectV2>
+//         </WalletProvider>
+//     );
+// };
